7056fb139f9f54d73c79150e171e6fbe42d39d78
DiamondCoder <diamond.coder.backup@gmail.com>
2023-11-09 15:20:55 +0100

    fuck this shit, I'm out

--- masm611/A_MENTES/Lesson9Test/Kovacs2.asm
+++ masm611/A_MENTES/Lesson9Test/Kovacs2.asm
@@ -32,6 +32,10 @@ main proc
 	INT 21h 
 main endp
 
+mov  ax, 5     ; ax = 5
+mov  cx, 10    ; cx = 10
+mul  cx        ; ax = ax * cx   ; actually dx:ax = ax * cx
+
 read_decimal proc 
 	PUSH AX ;AX mentése a verembe 
 	PUSH BX ;BX mentése a verembe 
